c552a3c3d387c656e5c26703147fd05f71f74d71
Jason Crist <jcrist@pbking.com>
2025-04-15 08:32:55 -0400

    Linted code

--- src/query-picker/edit.jsx
+++ src/query-picker/edit.jsx
@@ -1,113 +1,133 @@
-import { addFilter } from "@wordpress/hooks";
-import { InspectorControls } from "@wordpress/block-editor";
-import { PanelBody, FormTokenField  } from "@wordpress/components";
-import { registerBlockVariation } from "@wordpress/blocks";
-import { useSelect } from "@wordpress/data";
-import { useEffect, useRef } from "@wordpress/element";
+import { addFilter } from '@wordpress/hooks';
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, FormTokenField } from '@wordpress/components';
+import { registerBlockVariation } from '@wordpress/blocks';
+import { useSelect } from '@wordpress/data';
+import { useEffect, useRef } from '@wordpress/element';
 
-registerBlockVariation("core/query", {
-	name: "twenty-bellows/query-picker",
-	title: "Query Picker",
-	description: "A Query Loop variation that allows user to pick specific posts.",
-	isActive: ["namespace"],
+registerBlockVariation( 'core/query', {
+	name: 'twenty-bellows/query-picker',
+	title: 'Query Picker',
+	description:
+		'A Query Loop variation that allows user to pick specific posts.',
+	isActive: [ 'namespace' ],
 	attributes: {
-		namespace: "twenty-bellows/query-picker",
-		className: "is-query-picker",
+		namespace: 'twenty-bellows/query-picker',
+		className: 'is-query-picker',
 	},
-	scope: ["inserter", "transform"],
-	keywords: ["query"],
-	innerBlocks: [["core/post-template", {}, [["core/post-title", {}]]]],
+	scope: [ 'inserter', 'transform' ],
+	keywords: [ 'query' ],
+	innerBlocks: [
+		[ 'core/post-template', {}, [ [ 'core/post-title', {} ] ] ],
+	],
 	transforms: {
 		from: [
 			{
-				type: "block",
-				blocks: ["core/query"],
-				transform: (attributes, innerBlocks ) => {
-					return createBlock("twenty-bellows/query-picker", attributes, innerBlocks);
-				}
+				type: 'block',
+				blocks: [ 'core/query' ],
+				transform: ( attributes, innerBlocks ) => {
+					return createBlock(
+						'twenty-bellows/query-picker',
+						attributes,
+						innerBlocks
+					);
+				},
 			},
-		]
-	}
-});
+		],
+	},
+} );
 
-const withQueryPickerControls = (BlockEdit) => (props) => {
-
+const withQueryPickerControls = ( BlockEdit ) => ( props ) => {
 	if ( props.attributes?.namespace !== 'twenty-bellows/query-picker' ) {
-		return <BlockEdit {...props} />
+		return <BlockEdit { ...props } />;
 	}
 
 	const { attributes, setAttributes } = props;
 	const queryPostType = attributes?.query?.postType || 'post';
 	const pickedPosts = attributes?.query?.pickedPosts || [];
-	const initialPickedPostsRef = useRef(queryPostType);
+	const initialPickedPostsRef = useRef( queryPostType );
 
-	useEffect(() => {
-		if (initialPickedPostsRef.current !== queryPostType) {
-			setAttributes({
+	useEffect( () => {
+		if ( initialPickedPostsRef.current !== queryPostType ) {
+			setAttributes( {
 				query: {
 					...attributes.query,
 					pickedPosts: [],
-				}
-			})
+				},
+			} );
 		}
-	}, [attributes?.query?.postType]);
+	}, [ attributes?.query?.postType ] );
 
 	// Fetch posts based on the query post type
-	const posts = useSelect( (select) =>
-		select("core").getEntityRecords("postType", queryPostType, { per_page: -1 }),
-		[attributes?.query?.postType]
+	const posts = useSelect(
+		( select ) =>
+			select( 'core' ).getEntityRecords( 'postType', queryPostType, {
+				per_page: -1,
+			} ),
+		[ attributes?.query?.postType ]
 	);
 
 	// Build an array of options to populate the FormTokenField
-  	const postOptions = (posts ?? [])
-		.map( ( post ) => ( post.title.rendered ) )
-  		.sort( ( a, b ) => a.localeCompare( b ) );
+	const postOptions = ( posts ?? [] )
+		.map( ( post ) => post.title.rendered )
+		.sort( ( a, b ) => a.localeCompare( b ) );
 
 	// Map selected post IDs to their titles for the FormTokenField
-	const pickedPostsLabels = posts ? pickedPosts.map( ( postId ) => {
-		const post = posts.find( ( post ) => post.id === postId );
-		return post ? post.title.rendered : null;
-	}) : [];
+	const pickedPostsLabels = posts
+		? pickedPosts.map( ( postId ) => {
+				const post = posts.find( ( post ) => post.id === postId );
+				return post ? post.title.rendered : null;
+		  } )
+		: [];
 
 	const onSetPosts = ( values ) => {
-
 		pickedPostsLabels.length = 0;
 		attributes.query.pickedPosts = [];
 
 		if ( ! values || values.length === 0 ) {
-			setAttributes( { query: { ...attributes.query} } );
+			setAttributes( { query: { ...attributes.query } } );
 			return;
 		}
 
 		pickedPostsLabels.push( ...values );
 
 		attributes.query.pickedPosts = values.map( ( value ) => {
-			const post = posts.find( ( post ) => post.title.rendered === value );
+			const post = posts.find(
+				( post ) => post.title.rendered === value
+			);
 			return post ? post.id : null;
-		});
+		} );
 
-		setAttributes( { query: {
-			...attributes.query,
-		}});
-	}
+		setAttributes( {
+			query: {
+				...attributes.query,
+			},
+		} );
+	};
 
-	return <>
-		<BlockEdit {...props} />
-		<InspectorControls>
-			<PanelBody title="Query Options">
-			<FormTokenField
-  				__next40pxDefaultSize
-   				__nextHasNoMarginBottom
-				__experimentalExpandOnFocus
-				__experimentalShowHowTo = {false}
-   				label={ queryPostType.charAt(0).toUpperCase() + queryPostType.slice(1) + "s" }
-   				onChange={onSetPosts}
-   				suggestions={ postOptions }
-  				value={pickedPostsLabels}
- 			/>
-		</PanelBody>
-		</InspectorControls>
-	</>
+	return (
+		<>
+			<BlockEdit { ...props } />
+			<InspectorControls>
+				<PanelBody title="Query Options">
+					<FormTokenField
+						__next40pxDefaultSize
+						__nextHasNoMarginBottom
+						__experimentalExpandOnFocus
+						__experimentalShowHowTo={ false }
+						label={
+							queryPostType.charAt( 0 ).toUpperCase() +
+							queryPostType.slice( 1 ) +
+							's'
+						}
+						onChange={ onSetPosts }
+						suggestions={ postOptions }
+						value={ pickedPostsLabels }
+					/>
+				</PanelBody>
+			</InspectorControls>
+		</>
+	);
 };
 
-addFilter("editor.BlockEdit", "core/query", withQueryPickerControls);
+addFilter( 'editor.BlockEdit', 'core/query', withQueryPickerControls );
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-const path = require('path');
-const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-defaultConfig[0] = {
-	...defaultConfig[0],
+defaultConfig[ 0 ] = {
+	...defaultConfig[ 0 ],
 	...{
 		entry: {
 			queryPickerEdit: './src/query-picker/edit.jsx',
